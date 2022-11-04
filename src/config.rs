d3dc39b0e31177cdb94d518977575332a0280b40
Jay Oster <jay@kodewerx.org>
2022-11-04 14:23:52 -0700

    Move all configuration code to a new module
    - The ConfigState stores the directory where config files will be saved,
      but config reading/writing isn't implemented yet.
    - The save_config system only updates the in-memory configuration.

---
+++ src/config.rs
@@ -0,0 +1,113 @@
+use crate::consts::{APP_NAME, HEIGHT, WIDTH_STANDARD, WIDTH_ULTRAWIDE, WIDTH_WIDE};
+use bevy::{log::LogSettings, prelude::*, utils::tracing::Level};
+use directories::ProjectDirs;
+
+#[derive(Debug)]
+pub struct ConfigPlugin;
+
+#[derive(Debug)]
+pub struct ConfigState {
+    dirs: ProjectDirs,
+    ar: AspectRatio,
+    log_config: LogConfig,
+}
+
+#[derive(Debug)]
+pub enum SaveEvent {
+    /// Screen aspect ratio.
+    AspectRatio(AspectRatio),
+}
+
+#[derive(Copy, Clone, Debug)]
+pub enum AspectRatio {
+    Standard,
+    Wide,
+    Ultrawide,
+}
+
+#[derive(Clone, Debug)]
+struct LogConfig {
+    level: Level,
+    filter: String,
+}
+
+impl Plugin for ConfigPlugin {
+    fn build(&self, app: &mut App) {
+        app.insert_resource(ConfigState::default())
+            .add_event::<SaveEvent>()
+            .add_system(save_config);
+    }
+}
+
+fn save_config(mut state: ResMut<ConfigState>, mut events: EventReader<SaveEvent>) {
+    for event in events.iter() {
+        match event {
+            SaveEvent::AspectRatio(ar) => {
+                state.ar = *ar;
+            }
+        }
+    }
+}
+
+impl Default for ConfigState {
+    fn default() -> Self {
+        let dirs =
+            ProjectDirs::from("com", "BlipJoy", APP_NAME).expect("Could not find home directory");
+
+        // TODO: Load state from the config file
+        let ar = AspectRatio::Standard;
+
+        #[cfg(not(feature = "optimize"))]
+        let level = Level::INFO;
+        #[cfg(feature = "optimize")]
+        let level = Level::ERROR;
+
+        // TODO: Load state from the config file
+        let level = std::env::var("LOG_LEVEL")
+            .map(|level| match level.as_str() {
+                "trace" => Level::TRACE,
+                "debug" => Level::DEBUG,
+                "info" => Level::INFO,
+                "warn" => Level::WARN,
+                "error" => Level::ERROR,
+                level => {
+                    eprintln!("Unknown log level: {level}");
+                    Level::INFO
+                }
+            })
+            .unwrap_or(level);
+        let filter = std::env::var("LOG_FILTER")
+            .unwrap_or_else(|_| "wgpu=error,symphonia=error".to_string());
+
+        let log_config = LogConfig { level, filter };
+
+        Self {
+            dirs,
+            ar,
+            log_config,
+        }
+    }
+}
+
+impl ConfigState {
+    pub fn aspect_ratio(&self) -> AspectRatio {
+        self.ar
+    }
+
+    pub fn screen_resolution(&self) -> (u32, u32) {
+        let width = match self.aspect_ratio() {
+            AspectRatio::Standard => WIDTH_STANDARD,
+            AspectRatio::Wide => WIDTH_WIDE,
+            AspectRatio::Ultrawide => WIDTH_ULTRAWIDE,
+        };
+
+        (width, HEIGHT)
+    }
+
+    pub fn log_settings(&self) -> LogSettings {
+        LogSettings {
+            level: self.log_config.level,
+            filter: self.log_config.filter.clone(),
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,30 @@
-use bevy::{log::LogSettings, prelude::*, utils::tracing::Level, window::WindowResizeConstraints};
+use crate::bitmap::BitmapPlugin;
+use crate::config::{ConfigPlugin, ConfigState};
+use bevy::{prelude::*, window::WindowResizeConstraints};
 use bevy_embedded_assets::EmbeddedAssetPlugin;
 use bevy_kira_audio::prelude::*;
 use bevy_pixels::prelude::*;
-use bitmap::BitmapPlugin;
 
 mod bitmap;
+mod config;
+mod consts;
 mod scenes;
 
 fn main() {
-    let (width, height) = screen_resolution();
-    let width_x2 = width as f32 * 2.0;
-    let height_x2 = height as f32 * 2.0;
+    let config = ConfigState::default();
+    let (width, height) = config.screen_resolution();
+
+    let window_width = width as f32 * 2.0;
+    let window_height = height as f32 * 2.0;
 
     App::new()
         .insert_resource(WindowDescriptor {
-            title: "Blitter Blaster".to_string(),
-            width: width_x2,
-            height: height_x2,
+            title: consts::APP_NAME.to_string(),
+            width: window_width,
+            height: window_height,
             resize_constraints: WindowResizeConstraints {
-                min_width: width_x2,
-                min_height: height_x2,
+                min_width: window_width,
+                min_height: window_height,
                 ..default()
             },
             // mode: bevy::window::WindowMode::BorderlessFullscreen,
@@ -27,10 +32,11 @@ fn main() {
             ..default()
         })
         .insert_resource(PixelsOptions { width, height })
-        .insert_resource(log_settings())
+        .insert_resource(config.log_settings())
         .add_plugins_with(DefaultPlugins, |group| {
             group.add_before::<bevy::asset::AssetPlugin, _>(EmbeddedAssetPlugin)
         })
+        .add_plugin(ConfigPlugin)
         .add_plugin(AudioPlugin)
         .add_plugin(PixelsPlugin)
         .add_plugin(BitmapPlugin)
@@ -39,50 +45,4 @@ fn main() {
         .add_state(scenes::GameState::Intro)
         .add_system(bevy::window::close_on_esc)
         .run();
-}
-
-fn log_settings() -> LogSettings {
-    #[cfg(not(feature = "optimize"))]
-    let level = Level::INFO;
-    #[cfg(feature = "optimize")]
-    let level = Level::ERROR;
-
-    let level = std::env::var("LOG_LEVEL")
-        .map(|level| match level.as_str() {
-            "trace" => Level::TRACE,
-            "debug" => Level::DEBUG,
-            "info" => Level::INFO,
-            "warn" => Level::WARN,
-            "error" => Level::ERROR,
-            level => {
-                eprintln!("Unknown log level: {level}");
-                Level::INFO
-            }
-        })
-        .unwrap_or(level);
-    let filter =
-        std::env::var("LOG_FILTER").unwrap_or_else(|_| "wgpu=error,symphonia=error".to_string());
-
-    LogSettings { level, filter }
-}
-
-fn screen_resolution() -> (u32, u32) {
-    // Define screen resolutions for aspect ratios 4:3, 16:9, and 21:9.
-    const WIDTH_4X3: u32 = 320;
-    const WIDTH_16X9: u32 = 427;
-    const WIDTH_21X9: u32 = 560;
-    const HEIGHT: u32 = 240;
-
-    // TODO: Get aspect ratio setting from configuration
-    let width = match std::env::var("SCREEN_RES")
-        .unwrap_or_else(|_| "0".to_string())
-        .as_str()
-    {
-        "0" => WIDTH_4X3,
-        "1" => WIDTH_16X9,
-        "2" => WIDTH_21X9,
-        _ => panic!("Unimplemented screen resolution; valid options are: 0,1,2"),
-    };
-
-    (width, HEIGHT)
 }
