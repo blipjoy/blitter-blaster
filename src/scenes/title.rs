f7a06e5f8476401f5d7cea3ea4b16afee14c5e9b
Jay Oster <jay@kodewerx.org>
2022-11-08 18:13:57 -0800

    Add layering to the title screen

--- src/scenes/title.rs
+++ src/scenes/title.rs
@@ -12,11 +12,26 @@ pub struct TitlePlugin;
 struct TitleScreen;
 
 #[derive(Component)]
-struct Background(f32);
+struct Background;
+
+struct Motion {
+    angle: f32,
+    magnitude: f32,
+}
+
+impl Default for Motion {
+    fn default() -> Self {
+        Self {
+            angle: 0.0,
+            magnitude: 64.0,
+        }
+    }
+}
 
 impl Plugin for TitlePlugin {
     fn build(&self, app: &mut App) {
-        app.add_system_set(SystemSet::on_enter(GameState::Title).with_system(Self::enter))
+        app.init_resource::<Motion>()
+            .add_system_set(SystemSet::on_enter(GameState::Title).with_system(Self::enter))
             .add_system_set(SystemSet::on_update(GameState::Title).with_system(Self::update))
             .add_system_set(SystemSet::on_exit(GameState::Title).with_system(Self::exit));
     }
@@ -39,18 +54,19 @@ impl TitlePlugin {
             .unwrap();
 
         // Spawn the background
-        let transform_bundle = TransformBundle::from_transform(Transform::default());
+        let transform = Transform::from_xyz(0.0, 0.0, 1.0);
+        let transform_bundle = TransformBundle::from_transform(transform);
         let image = io.load_path_sync(Path::new("images/bg1.png")).unwrap();
         commands
             .spawn()
             .insert(Bitmap::new(&image).tiled(true))
             .insert_bundle(transform_bundle)
-            .insert(Background(0.0))
+            .insert(Background)
             .insert(TitleScreen);
 
         // Spawn the title logo
         let x = (options.width / 2) as f32;
-        let transform = Transform::from_xyz(x - 120.0, 65.0, 1.0);
+        let transform = Transform::from_xyz(x - 120.0, 65.0, 2.0);
         let transform_bundle = TransformBundle::from_transform(transform);
         let image = io.load_path_sync(Path::new("images/odonata.png")).unwrap();
         commands
@@ -60,14 +76,21 @@ impl TitlePlugin {
             .insert(TitleScreen);
     }
 
-    fn update(mut query: Query<(&mut Transform, &mut Background)>, time: Res<Time>) {
+    fn update(
+        time: Res<Time>,
+        mut query: Query<&mut Transform, With<Background>>,
+        mut motion: ResMut<Motion>,
+    ) {
         let delta = time.delta().as_secs_f32();
 
-        for (mut transform, mut background) in &mut query {
-            let motion = Vec3::new(-background.0.cos(), background.0.sin(), 0.0);
-            transform.translation += motion * 56.0 * delta;
-            background.0 -= 0.000033;
+        let velocity = Quat::from_rotation_z(motion.angle) * Vec3::X * motion.magnitude;
+
+        for mut transform in &mut query {
+            let z = transform.translation.z;
+            transform.translation -= velocity * delta * z;
         }
+
+        motion.angle += 0.000033;
     }
 
     fn exit(mut commands: Commands, entities: Query<Entity, With<TitleScreen>>) {
